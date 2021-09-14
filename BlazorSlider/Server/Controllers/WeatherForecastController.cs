3b37b850de4de2958a482a1cc5118b6719e37296
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2021-09-13 23:18:48 -0600

    Agregar archivos de proyecto.

---
+++ BlazorSlider/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using BlazorSlider.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSlider.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "https://costarica21.com/Img/Beach-San-Josesito-GDEL-00.jpg",
+            "https://baraka.es/wp-content/uploads/2019/01/destacada-paisaje-costa-rica.jpg",
+            "https://i.pinimg.com/originals/48/54/df/4854dfc74b052e442e72884fae99517c.jpg",
+            "https://conozcasucanton.com/wp-content/uploads/sites/11/2016/10/Isla-del-Coco.jpg",
+            "https://costarica.org/wp-content/uploads/2017/02/Volcanoe.jpg"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ BlazorSlider/Shared/WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorSlider.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public string Summary { get; set; }
+    }
+}
